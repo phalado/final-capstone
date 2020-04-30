a8822279933fdaf3c091e775e6359403084447f0
phalado <phalado@gmail.com>
2020-04-30 11:34:28 -0300

    Trying to deploy to Netlify

---
+++ build/precache-manifest.e5df90664c5d0561959df1999040ab6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598cd7df9dce31a37ffeb3f3fa867ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c32ac170470ffaf9d9",
+    "url": "/static/css/main.ca803b1a.chunk.css"
+  },
+  {
+    "revision": "3be615df86a268fe3a4e",
+    "url": "/static/js/2.74162358.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.74162358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c32ac170470ffaf9d9",
+    "url": "/static/js/main.b039837d.chunk.js"
+  },
+  {
+    "revision": "ec7b0272a829513dcd32",
+    "url": "/static/js/runtime-main.bb41bf79.js"
+  }
+]);
